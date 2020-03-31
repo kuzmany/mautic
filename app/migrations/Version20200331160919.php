5018eae3af88975a7e53cc82656c851ce8ec0af8
Zdeno Kuzmany <zdeno@kuzmany.biz>
2020-03-31 18:20:59 +0200

    Add migration

---
+++ app/migrations/Version20200331160919.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ * @package     Mautic
+ * @copyright   2020 Mautic Contributors. All rights reserved.
+ * @author      Mautic
+ * @link        http://mautic.org
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+namespace Mautic\Migrations;
+
+use Doctrine\DBAL\Migrations\SkipMigrationException;
+use Doctrine\DBAL\Schema\Schema;
+use Mautic\CoreBundle\Doctrine\AbstractMauticMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20200331160919 extends AbstractMauticMigration
+{
+    /**
+     * @param Schema $schema
+     *
+     * @throws SkipMigrationException
+     * @throws \Doctrine\DBAL\Schema\SchemaException
+     */
+    public function preUp(Schema $schema)
+    {
+        $formTable   = $schema->getTable($this->prefix.'forms');
+        $fieldsTable = $schema->getTable($this->prefix.'form_fields');
+
+        if ($formTable->hasColumn('progressive_profiling') && $formTable->hasColumn('progressive_profiling_limit') && $fieldsTable->hasColumn('always_display')) {
+            throw new SkipMigrationException('Schema includes this migration');
+        }
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $formTable = $schema->getTable($this->prefix.'forms');
+
+        if (!$formTable->hasColumn('progressive_profiling')) {
+            $this->addSql('ALTER TABLE '.$this->prefix.'forms ADD progressive_profiling tinyint(1) DEFAULT NULL');
+        }
+        if (!$formTable->hasColumn('progressive_profiling_limit')) {
+            $this->addSql('ALTER TABLE '.$this->prefix.'forms ADD progressive_profiling_limit INT(11)  NOT NULL DEFAULT 0;');
+        }
+
+        $fieldsTable = $schema->getTable($this->prefix.'form_fields');
+
+        if (!$formTable->hasColumn('always_display')) {
+            $this->addSql('ALTER TABLE '.$this->prefix.'forms ADD always_display tinyint(1) DEFAULT NULL');
+        }
+    }
+}
