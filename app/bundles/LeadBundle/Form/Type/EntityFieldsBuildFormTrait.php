0f5ff93d3d1d3b43769a1a893f25c799dd83c906
Zdeno Kuzmany <zdeno@kuzmany.biz>
2020-10-14 18:05:18 +0200

    Add TextareaType::class related to M3

--- app/bundles/LeadBundle/Form/Type/EntityFieldsBuildFormTrait.php
+++ app/bundles/LeadBundle/Form/Type/EntityFieldsBuildFormTrait.php
@@ -28,6 +28,7 @@ use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -270,7 +271,7 @@ trait EntityFieldsBuildFormTrait
                             $constraints[] = new Length(['max' => 65535]);
                             break;
 
-                        case 'textarea':
+                        case TextareaType::class:
                             if (!empty($properties['allowHtml'])) {
                                 $cleaningRules[$field['alias']] = 'html';
                             }
